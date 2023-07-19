0265ac1b03a98f64f427e4bedfd1b794c9bcc3a7
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2023-07-18 22:10:06 -0300

    Modificando sistema para criar metodo de cadastro

--- appFirebase/src/firebaseConnection.js
+++ appFirebase/src/firebaseConnection.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 import 'firebase/database'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDN7AYcsslkcmIaLywMBFBQOB6M34kLHn4",
   authDomain: "meuapp-b7098.firebaseapp.com",
